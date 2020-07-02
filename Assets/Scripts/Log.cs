37069382f8d553e9d36c2784bc8f3c709e64f60d
mickeyhir <altruistccc@gmail.com>
2020-07-02 15:26:57 +0300

    Fixing bugs

--- Assets/Scripts/Log.cs
+++ Assets/Scripts/Log.cs
@@ -10,33 +10,50 @@ public class Log : MonoBehaviour
     public GameObject lava;
     public GameObject explosionPrefab;
     public GameMaster gameMaster;
+    public float delayTime;
+
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
         hj = this.GetComponent<HingeJoint>();
     }
 
-    public void Swing(float dir)
+    public void SwingLeft()
     {
-        Vector3 forceDir = new Vector3(0, 0, force * dir);
+        Vector3 forceDir = new Vector3(0, 0, force);
+        rb.AddForce(forceDir);
+    }
+    public void SwingRight()
+    {
+        Vector3 forceDir = new Vector3(0, 0, -force);
         rb.AddForce(forceDir);
     }
 
     public void Release()
     {
+        rb.AddForce(Vector3.up);
         hj.breakForce = 0;
         gameMaster.buttonCanvas.SetActive(false);
+        StartCoroutine(StuckHendler());
     }
 
+    IEnumerator StuckHendler()
+    {
+        yield return new WaitForSeconds(delayTime);
+        Explode();
+    }
     private void OnCollisionEnter(Collision other)
     {
         if(other.gameObject == lava)
         {
-            //create explosion effect
-            GameObject explosionEffect = (GameObject)Instantiate(explosionPrefab, transform.position, Quaternion.identity);
-            Destroy(explosionEffect, 5f);
-            //call for restart
-            gameMaster.RestartGame();
+            Explode();         
         }
+    }
+
+    public void Explode()
+    {
+        GameObject explosionEffect = (GameObject)Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(explosionEffect, 5f);
+        gameMaster.RestartGame();
     }
 }
