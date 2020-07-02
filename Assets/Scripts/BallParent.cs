a479a26be17c811dbd9bac95fe98769d4951915a
mickeyhir <altruistccc@gmail.com>
2020-07-02 09:51:53 +0300

    Basic logic and animation

---
+++ Assets/Scripts/BallParent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallParent : MonoBehaviour
+{
+    public Vector3 startPosition;
+    public Vector3 endPosition;
+    public float speed;
+    void Start()
+    {
+        transform.position = startPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, endPosition, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Log.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : MonoBehaviour
+{
+    private Rigidbody rb;
+    private HingeJoint hj;
+    public float force;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        hj = this.GetComponent<HingeJoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Swing(float dir)
+    {
+        Vector3 forceDir = new Vector3(0, 0, force * dir);
+        rb.AddForce(forceDir);
+    }
+
+    public void Release()
+    {
+        hj.breakForce = 0;
+    }
+}
